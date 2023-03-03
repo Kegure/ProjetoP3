a3194f1c78e019bf01cb6023425bf9c2fc096d98
Kegure <robsongabriarc@hotmail.com>
2023-03-03 02:45:24 -0300

    Criando package de Registro

---
+++ CinemaProj/src/main/java/com/P3/CinemaProj/registration/RegistrationService.java
@@ -0,0 +1,7 @@
+package com.P3.CinemaProj.registration;
+
+public class RegistrationService {
+    public String register(RegistrationRequest request) {
+        return null;
+    }
+}
---
+++ CinemaProj/src/main/java/com/P3/CinemaProj/registration/UserRegistrationController.java
@@ -0,0 +1,17 @@
+package com.P3.CinemaProj.registration;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path ="api/v1/registration")
+@AllArgsConstructor
+public class UserRegistrationController {
+    private RegistrationService registrationService;
+
+    public String register(@RequestBody RegistrationRequest request){
+        return registrationService.register(request);
+    }
+}
