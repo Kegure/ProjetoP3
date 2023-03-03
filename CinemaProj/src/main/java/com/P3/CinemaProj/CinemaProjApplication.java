1b85f8069b94577bf2efc0a9c7d1ed3d3d12645b
Robson Gabriel <34819099+Kegure@users.noreply.github.com>
2023-03-02 21:22:43 -0300

    First Commit

---
+++ CinemaProj/src/main/java/com/P3/CinemaProj/CinemaProjApplication.java
@@ -0,0 +1,13 @@
+package com.P3.CinemaProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaProjApplication.class, args);
+	}
+
+}
---
+++ CinemaProj/src/test/java/com/P3/CinemaProj/CinemaProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.P3.CinemaProj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
