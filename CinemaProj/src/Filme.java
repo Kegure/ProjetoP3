42a6541abc90642e037d618b6bf7006205e8496e
Ricardo André <91620783+rickzerahh@users.noreply.github.com>
2023-03-25 12:02:56 -0300

    Create Filme.java

---
+++ CinemaProj/src/Filme.java
@@ -0,0 +1,39 @@
+// Filme - É a classe que representa o filme exibido na sessão da sala, ela deve
+// ter os seguintes atributos: nome, duração, sinopse, valor.
+
+package ProjetoCinema;
+
+public class Filme {
+    private String nome;
+    private int duracao;
+    private String sinopse;
+    private double valor;
+
+    public Filme(String nome){
+        this.nome = nome;
+    }
+    public String getNome(){
+        return nome;
+    }
+    public int getDuracao(){
+        return duracao;
+    }
+    public String getSinopse(){
+        return sinopse;
+    }
+    public double getValor(){
+        return valor;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public void setDuracao(int duracao){
+        this.duracao = duracao;
+    }
+    public void setSinopse(String sinopse){
+        this.sinopse = sinopse;
+    }
+    public void setValor(double valor){
+        this.valor = valor;
+    }
+}
